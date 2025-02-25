d1a0ea40eb1b4ac8b0cfc6bb03791c680b107dec
michaelfournier064 <michaelfournier064@outlook.com>
2025-02-25 17:13:03 -0500

    Initial commit msg

---
+++ HelloWorld/src/AboutMe.java
@@ -0,0 +1,11 @@
+
+public class AboutMe {
+
+	public static void main(String[] args) {
+		System.out.println("Michael Fournier");
+		System.out.println("Computer Science");
+		System.out.println("I would like to get to the point in my carrer and abilities where creating my own software ideas for a living is possible");
+		System.out.println("Skiing and video games are my main hobbies");
+	}
+
+}
