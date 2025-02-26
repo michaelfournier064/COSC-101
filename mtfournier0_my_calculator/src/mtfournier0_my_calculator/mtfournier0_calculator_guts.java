2adc71053016b2c508a59c0c08232eb05c717645
michaelfournier064 <michaelfournier064@outlook.com>
2025-02-26 15:19:11 -0500

    fix(mtfournier0_calculator_guts): Variable name change

--- mtfournier0_my_calculator/src/mtfournier0_my_calculator/mtfournier0_calculator_guts.java
+++ mtfournier0_my_calculator/src/mtfournier0_my_calculator/mtfournier0_calculator_guts.java
@@ -5,28 +5,28 @@ import static mtfournier0_my_calculator.mtfournier0_calculator_interface.numberC
 import mtfournier0_my_calculator.mtfournier0_calculator_interface.OperationData;
 
 public class mtfournier0_calculator_guts {
-    public static void addition(double a, double b) {
-        System.out.println("Result (Addition): " + (a + b));
+    public static void addition(double input1, double input2) {
+        System.out.println("Result (Addition): " + (input1 + input2));
     }
     
-    public static void subtraction(double a, double b) {
-        System.out.println("Result (Subtraction): " + (a - b));
+    public static void subtraction(double input1, double input2) {
+        System.out.println("Result (Subtraction): " + (input1 - input2));
     }
     
-    public static void multiplication(double a, double b) {
-        System.out.println("Result (Multiplication): " + (a * b));
+    public static void multiplication(double input1, double input2) {
+        System.out.println("Result (Multiplication): " + (input1 * input2));
     }
     
-    public static void division(double a, double b) {
-        if (b != 0) {
-            System.out.println("Result (Division): " + (a / b));
+    public static void division(double input1, double input2) {
+        if (input2 != 0) {
+            System.out.println("Result (Division): " + (input1 / input2));
         } else {
             System.out.println("Error: Division by zero.");
         }
     }
     
-    public static void modulus(double a, double b) {
-        System.out.println("Result (Modulus): " + (a % b));
+    public static void modulus(double input1, double input2) {
+        System.out.println("Result (Modulus): " + (input1 % input2));
     }
     
     public static void calculator_single_instance() {
